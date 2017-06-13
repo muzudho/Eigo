48a112ac6e868c9b0179fe2052d593582ffef313
muzudho <muzudho1@gmail.com>
2017-06-14 05:26:19 +0900

    上げ直し

---
+++ Source/Eigo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Eigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("(^_^)？ ");
+            for (;;)
+            {
+                string line = Console.In.ReadLine();
+
+                switch (line.Trim())
+                {
+                    case "bye": return;
+                    default:
+                        Console.Out.WriteLine("> " + line);
+                        Console.Out.WriteLine("(^_^)ファット？ ");
+                        break;
+                }
+            }
+        }
+    }
+}
